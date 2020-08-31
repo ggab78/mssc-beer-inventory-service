0ea6321e1631524254dfd9779ced6998d7b0e0de
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-08-31 22:08:43 +0200

    add allocation service

---
+++ src/main/java/com/gabriel/beerservice/services/AllocationService.java
@@ -0,0 +1,8 @@
+package com.gabriel.beerservice.services;
+
+import com.gabriel.model.BeerOrderDto;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}

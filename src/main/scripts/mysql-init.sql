575452dae650a8e93ad15ef3c01bd56dd1ab3225
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-10-23 16:35:25 +0200

    add properties for docker active profile

--- src/main/scripts/mysql-init.sql
+++ src/main/scripts/mysql-init.sql
@@ -5,3 +5,16 @@ CREATE USER IF NOT EXISTS `beer_inventory_service`@`%` IDENTIFIED WITH mysql_nat
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
 CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `beerinventoryservice`.* TO `beer_inventory_service`@`%`;
 FLUSH PRIVILEGES;
+
+USE beerinventoryservice;
+
+CREATE TABLE `beer_inventory` (
+                                `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                `created_date` datetime(6) DEFAULT NULL,
+                                `last_modified_date` datetime(6) DEFAULT NULL,
+                                `version` bigint(20) DEFAULT NULL,
+                                `beer_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+                                `quantity_on_hand` int(11) DEFAULT NULL,
+                                `upc` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                                PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
